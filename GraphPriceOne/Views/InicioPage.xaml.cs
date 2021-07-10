5807cb838df831d88f46db9344813c1671558431
Humberto Molina López <humberto_molinalopez@hotmail.com>
2021-07-09 19:34:33 -0500

    archivos para crear pa pagina de inicio y popup de pegar url

--- GraphPriceOne/Views/InicioPage.xaml.cs
+++ GraphPriceOne/Views/InicioPage.xaml.cs
@@ -4,6 +4,19 @@ using GraphPriceOne.ViewModels;
 
 using Microsoft.UI.Xaml.Controls;
 
+using HtmlAgilityPack;
+
+using Windows.ApplicationModel.DataTransfer;
+
+using System.Web;
+
+using System.Net.Http;
+
+using System.Threading.Tasks;
+
+using System;
+using Microsoft.UI.Xaml;
+
 namespace GraphPriceOne.Views
 {
     public sealed partial class InicioPage : Page
@@ -14,6 +27,145 @@ namespace GraphPriceOne.Views
         {
             ViewModel = Ioc.Default.GetService<InicioViewModel>();
             InitializeComponent();
+        }
+        const string titleNode = "//h1[@class='ui-pdp-title']";
+        const string titleNode1 = "//span[@id='productTitle']";
+
+        const string priceNodeV1 = "//span[@class='price-tag-fraction']";
+        const string priceNodeV2 = "//span[@id='priceblock_ourprice']";
+        const string priceNodeV3 = "//span[@id='priceblock_saleprice']";
+        const string error = "Error: No ha seleccionado un selector para esta pagina";
+        const string errorTitle = "Error";
+        public static async Task<string> OutputClipboardText()
+        {
+            DataPackageView dataPackageView = Clipboard.GetContent();
+
+            if (dataPackageView.Contains(StandardDataFormats.Text))
+            {
+                string text = await dataPackageView.GetTextAsync();
+                return text;
+            }
+            return await Task.FromResult(String.Empty);
+        }
+        private async void DetectarUrlValidaClipboard()
+        {
+            string url = await OutputClipboardText();
+            //detecta que la url es valida y lanza el dialogo de confirmacion
+            if (Uri.IsWellFormedUriString(url, UriKind.Absolute) && url != AmazonURL.Text)
+            {
+                ShowDialog();
+                //FetchPrice.Focus(FocusState.Programmatic);
+            }
+        }
+        public async void validarDatos()
+        {
+            // Create an HttpClientHandler object and set to use default credentials
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.UseDefaultCredentials = true;
+
+            // Create an HttpClient object
+            HttpClient httpClient = new HttpClient(handler);
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(AmazonURL.Text);
+                HttpContent content = response.Content;
+                HtmlDocument document = new HtmlDocument();
+                document.LoadHtml(await content.ReadAsStringAsync());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", ex.Message);
+            }
+            handler.Dispose();
+            httpClient.Dispose();
+        }
+
+        // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+        static readonly HttpClient client = new HttpClient();
+        public async void ObtenerDatos()
+        {
+            try
+            {
+                Progress.IsActive = true;
+                Price.Text = "";
+                Title.Text = "";
+                Price.FontSize = 204;
+                string URLTexto = AmazonURL.Text;
+                HttpResponseMessage response = await client.GetAsync(URLTexto);
+                HttpContent content = response.Content;
+                HtmlDocument document = new HtmlDocument();
+                document.LoadHtml(await content.ReadAsStringAsync());
+
+                Task.WaitAll();
+                // Get Title
+                var title = document.DocumentNode.SelectSingleNode(titleNode);
+                var title1 = document.DocumentNode.SelectSingleNode(titleNode1);
+                // Get and Show Price
+                var price = document.DocumentNode.SelectSingleNode(priceNodeV1);
+                Progress.IsActive = false;
+                if (price == null)
+                    price = document.DocumentNode.SelectSingleNode(priceNodeV2);
+                // Show Title and Price
+                if (price == null)
+                    price = document.DocumentNode.SelectSingleNode(priceNodeV3);
+                // Show Title and Price
+                if (title != null)
+                    Title.Text = HttpUtility.HtmlDecode(title.InnerText).Trim();
+                else if (title != null)
+                {
+                    Title.Text = HttpUtility.HtmlDecode(title1.InnerText).Trim();
+                }
+                else
+                    Title.Text = errorTitle;
+                if (price != null)
+                    Price.Text = price.InnerText;
+                else
+                    Title.Text = error;
+            }
+            catch (Exception ex)
+            {
+                Progress.IsActive = false;
+                Title.Text = "Porfavor agregue una url valida";
+                Price.FontSize = 16;
+                Price.Text = ex.ToString() + "\n";
+            }
+        }
+        private void FetchPrice_Click(object sender, RoutedEventArgs e)
+        {
+            ObtenerDatos();
+        }
+        private async void ShowDialog()
+        {
+            ContentDialog dialog = new ContentDialog();
+            dialog.Title = "Desea agregar esta url?";
+            dialog.PrimaryButtonText = "Si";
+            dialog.SecondaryButtonText = "No, gracias";
+            dialog.CloseButtonText = "Cancel";
+            dialog.DefaultButton = ContentDialogButton.Primary;
+            dialog.Content = new ContentDialogContent();
+            try
+            {
+                var result = await dialog.ShowAsync();
+
+                //AmazonURL.Text = result.ToString();
+                if (result.ToString() == "Primary")
+                {
+                    AmazonURL.Text = await OutputClipboardText();
+                    ObtenerDatos();
+
+                }
+                //AmazonURL.Text = await OutputClipboardText();
+            }
+            catch (Exception ex)
+            {
+                Price.FontSize = 16;
+                Price.Text = ex.ToString() + "\n";
+            }
+        }
+        private void AmazonURL_PointerEntered(object sender, RoutedEventArgs e)
+        {
+            DetectarUrlValidaClipboard();
         }
     }
 }
