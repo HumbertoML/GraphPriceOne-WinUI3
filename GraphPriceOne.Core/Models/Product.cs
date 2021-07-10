57022713f8abf80b76197049b0cc6f90455e26f4
Humberto Molina López <humberto_molinalopez@hotmail.com>
2021-07-09 21:24:59 -0500

    Creando clase de productos

---
+++ GraphPriceOne.Core/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphPriceOne.Core.Models
+{
+    public class Product
+    {
+        public long ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        //precio normal
+        public string PriceTag { get; set; }
+
+        //precio con descuento
+        public string PriceDesc { get; set; }
+
+        public string PriceDescPorc { get; set; }
+
+        public DateTime ProductTimePrice { get; set; }
+
+        // transporte gratis o de paga etc
+        public double ShippingPrice { get; set; }
+
+        public string Store { get; set; }
+    }
+}
