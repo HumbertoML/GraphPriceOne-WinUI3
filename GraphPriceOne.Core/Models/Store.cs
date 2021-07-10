cf71f5e2dea1b2886ddaff07b69f4c990a4257fa
Humberto Molina López <humberto_molinalopez@hotmail.com>
2021-07-09 19:55:25 -0500

    Creando clase store

---
+++ GraphPriceOne.Core/Models/Store.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphPriceOne.Core.Models
+{
+    // Remove this class once your pages/features are using your data.
+    // This is used by the SampleDataService.
+    // It is the model class we use to display data on pages like Grid, Chart, and ListDetails.
+    public class Store
+    {
+        public string StoreID { get; set; }
+
+        public string StoreName { get; set; }
+
+        public ICollection<SampleOrder> Orders { get; set; }
+    }
+}
