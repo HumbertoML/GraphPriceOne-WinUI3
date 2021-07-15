55a93731c398065d0bd6d2ea63d0beac2370ca24
Humberto Molina López <humberto_molinalopez@hotmail.com>
2021-07-15 13:42:25 -0500

    Agregando comand handler

---
+++ GraphPriceOne/Library/CommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphPriceOne.Library
+{
+    internal class CommandHandler : ICommand
+    {
+        private Action _action;
+        public event EventHandler CanExecuteChanged;
+
+        public CommandHandler(Action action)
+        {
+            _action = action;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
