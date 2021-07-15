00fe7f81b9e47f90a44f857b307b4d7644a30014
Humberto Molina López <humberto_molinalopez@hotmail.com>
2021-07-15 13:42:02 -0500

    Creando conexion a base de datos

---
+++ Connection/Connections.cs
@@ -0,0 +1,27 @@
+﻿using LinqToDB.Data;
+using LinqToDB.DataProvider.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using LinqToDB.Mapping;
+using Models;
+using LinqToDB;
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Connection
+{
+    public class Connections : DataConnection
+    {
+        //conexion a nuestra base de datos con sql server
+        public Connections() : base(new SqlServerDataProvider("", SqlServerVersion.v2017),
+            "Data Source=DESKTOP-R56EJQK\\SQLEXPRESS;Database=GraphPriceOne;Integrated Security=True;")
+        {
+
+        }
+        public ITable<TProducts> Products => GetTable<TProducts>();
+    }
+}
