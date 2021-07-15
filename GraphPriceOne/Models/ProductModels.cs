4fe6f03c27291ba46a2615e5713250a0a39aa235
Humberto Molina López <humberto_molinalopez@hotmail.com>
2021-07-15 13:44:38 -0500

    Model de productos para retornar y guardar valores

---
+++ GraphPriceOne/Models/ProductModels.cs
@@ -0,0 +1,23 @@
+﻿using GraphPriceOne.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Email;
+
+namespace GraphPriceOne.Models
+{
+    public class ProductModels : PropertyChangedNotification
+    {
+        public int ID { get; set; }
+        public string TxtUrl
+        {
+            get { return GetValue(() => TxtUrl); }
+            set
+            {
+                SetValue(() => TxtUrl, value);
+            }
+        }
+    }
+}
